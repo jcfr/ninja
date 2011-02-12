e7c5db5f10aaebc26e798d45ce3f3d9a1c83b425
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-02-12 07:26:17 -0500

    Add open(), mkstemp() and PATH_MAX to win_compat.h

--- src/win/ninja_win_compat.h
+++ src/win/ninja_win_compat.h
@@ -2,10 +2,15 @@
 #ifndef __win_compat_h
 #define __win_compat_h
 
-#include <io.h> // For _close
+#include <io.h> // For _close, _open() and _mktemp_s()
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <direct.h> // For _mkdir and _getcwd
 
 typedef int ssize_t;
+
+#define PATH_MAX FILENAME_MAX
 
 int close(int fd)
 {
@@ -25,6 +30,21 @@ char * getcwd (char *__buf, size_t __size)
 int isatty(int fd)
 {
   return _isatty(fd);
+}
+
+int open(const char* filename)
+{
+  return _open(filename, _O_RDWR | _O_TEXT);
+}
+
+int mkstemp (char *__template)
+{
+  int ret = _mktemp_s(__template, sizeof(__template));
+  if(ret != 0)
+    {
+    return -1;
+    }
+  return open(__template);
 }
 
 #endif
